1f1f3535f339d01101805314235bb91a72072211
Sanjay Vikram <136677368+SanjayVikram22@users.noreply.github.com>
2024-08-12 19:35:17 +0530

    Update sever.py

--- sever.py
+++ sever.py
@@ -3,12 +3,13 @@ from flask_cors import CORS
 import pickle
 import numpy as np
 
-scaler =pickle.load(open('./project/scaler-2.pkl','rb'))
-model =pickle.load(open('./project/loan_rf_model-2.pkl','rb'))
+scaler = pickle.load(open('./project/scaler-2.pkl', 'rb'))
+model = pickle.load(open('./project/loan_rf_model-2.pkl', 'rb'))
 
 
 app = Flask(__name__)
 CORS(app)
+
 
 @app.route('/api/sendMessage', methods=['POST'])
 def send_message():
@@ -18,27 +19,30 @@ def send_message():
     employee_status = data.get('employeeStatus')
     marital_status = data.get('maritalStatus')
     dependents = data.get('dependents')
-    income=data.get('monthlyIncome')
-    req_loan_amt = data.get('reqLoanAmt')
-    credit_hist = data.get('creditHist')
-    loan_repayment =  int(data.get('loanRepayment'))*12
-    monthlyexpenses=data.get('monthlyExpenses')
-    remaining=int(income)-int(monthlyexpenses)
-    emi=(int(req_loan_amt)*int(loan_repayment))/1200
-    
+    income = data.get('monthlyIncome')
+    req_loan_amt = float(data.get('reqLoanAmt', 0))
+    credit_hist = float(data.get('creditHist', 0))
+    loan_repayment = float(data.get('loanRepayment', 0))
+    monthlyexpenses = float(data.get('monthlyExpenses', 0))
+    remaining = int(income)-int(monthlyexpenses)
+    emi = (int(req_loan_amt)*int(loan_repayment))/1200
+
     features = np.array([[income, req_loan_amt, loan_repayment, monthlyexpenses, remaining, emi]])
+    print(features)
     features_scaled = scaler.transform(features)
     print(emi)
-    test_case= np.array([[gender,marital_status,dependents,education,employee_status,features_scaled[0][0],features_scaled[0][1],features_scaled[0][2],credit_hist,features_scaled[0][3],features_scaled[0][4],features_scaled[0][5]]])
+    test_case = np.array([[gender, marital_status, dependents, education, employee_status, features_scaled[0][0], features_scaled[0][1], features_scaled[0][2], credit_hist, features_scaled[0][3], features_scaled[0][4], features_scaled[0][5]]])
     print(test_case)
     prediction = model.predict(test_case)
-    if prediction[0]==0:
-        b=0
+    print(prediction)
+    if prediction[0] == 0:
+        b = 0
     else:
-        b=1
-    a=str(prediction[0])
-    
-    return jsonify({"status": "success", "message": "Form received","pred":a,"approval":b})
+        b = 1
+    a = str(prediction[0])
+
+    return jsonify({"status": "success", "message": "Form received", "pred": a, "approval": b})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
